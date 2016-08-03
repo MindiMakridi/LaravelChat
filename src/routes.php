ad4d0412944dc3d46351386b74fabede20d71087
youdontgettobringfriends <thehabibass@gmail.com>
2016-08-03 12:53:15 +0500

    route fix

--- src/routes.php
+++ src/routes.php
@@ -3,6 +3,3 @@
 Route::post(config('Chat')['Routes']['getMessages'], ['uses' => '\FrameworkTeam\Chat\MessageController@longpull']);
 Route::post(config('Chat')['Routes']['sendMessage'], ['uses' => '\FrameworkTeam\Chat\MessageController@send']);
 Route::post(config('Chat')['Routes']['deleteMessage'], ['uses' => '\FrameworkTeam\Chat\MessageController@deleteMessage']);
-
-
-Route::get('/frameworkteam/{id}', ['uses' => 'App\Http\Controllers\ChatController@index']);
